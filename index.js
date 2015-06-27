3ac674cc8e252381b3736802c3b02da98461ce3b
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-06-27 08:56:18 +0200

    Use `random-int` module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var alphabet = require('alphabet').lower;
+var randomInt = require('random-int');
 
 module.exports = function () {
-	return alphabet[Math.floor(Math.random() * alphabet.length)];
+	return alphabet[randomInt(0, 25)];
 };
