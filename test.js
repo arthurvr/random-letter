5eb94ca104d1f3a627ee3381ec1ba39039752760
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 22:50:51 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-'use strict';
-var test = require('ava');
-var randomLetter = require('./');
+import test from 'ava';
+import randomLetter from './';
 
-test('should return a random letter from the english alphabet', function (t) {
+test('should return a random letter from the english alphabet', t => {
 	t.is(randomLetter().length, 1);
 	t.is(typeof randomLetter(), 'string');
+
+	t.end();
 });
